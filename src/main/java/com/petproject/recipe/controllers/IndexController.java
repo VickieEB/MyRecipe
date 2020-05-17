e1cfad461e47477920366035c9fe0c05a29c29e1
VickieEB <victoria.etim.bassey@gmail.com>
2020-05-17 20:10:50 +0100

    Adding JPA Query methods for Category and UnitOfMeasure. Closes #8

--- src/main/java/com/petproject/recipe/controllers/IndexController.java
+++ src/main/java/com/petproject/recipe/controllers/IndexController.java
@@ -1,15 +1,32 @@
 package com.petproject.recipe.controllers;
 
+import com.petproject.recipe.domain.Category;
+import com.petproject.recipe.domain.UnitOfMeasure;
+import com.petproject.recipe.repositories.CategoryRepository;
+import com.petproject.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
-        System.out.println("1...2...3...4");
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Mexican");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Pinch");
+        System.out.println("Category id is: " + categoryOptional.get().getId());
+        System.out.println("UOM id is: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 
