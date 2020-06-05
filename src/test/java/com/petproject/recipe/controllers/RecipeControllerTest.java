ccb915ceb41f22e106473832956447de8a78b57f
VickieEB <victoria.etim.bassey@gmail.com>
2020-06-05 23:10:31 +0100

    Write Test for RecipeController using Mockito. Closes #12

--- src/test/java/com/petproject/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/com/petproject/recipe/controllers/RecipeControllerTest.java
@@ -1,17 +1,20 @@
 package com.petproject.recipe.controllers;
 
+import com.petproject.recipe.domain.Recipe;
 import com.petproject.recipe.service.RecipeServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class RecipeControllerTest {
 
@@ -31,10 +34,26 @@ public class RecipeControllerTest {
 
     @Test
     public void listRecipe() {
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        Recipe recipe2 = new Recipe();
+        recipe1.setId(1L);
+        recipe2.setId(2L);
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+
         String viewName = recipeController.listRecipe(model);
         assertEquals("recipe/index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+
 
     }
 }
