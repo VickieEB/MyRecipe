27bba3803871154509096f6ab86aa9e2e832a3cd
VickieEB <victoria.etim.bassey@gmail.com>
2020-08-17 21:46:51 +0100

    Display Error Message on page. Closes #31

--- src/main/java/com/petproject/recipe/controllers/RecipeController.java
+++ src/main/java/com/petproject/recipe/controllers/RecipeController.java
@@ -87,10 +87,11 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(){
-        log.error("Handling not found exception");
+    public ModelAndView handleNotFound( Exception exception){
+        log.error("Handling not found exception" + exception.getMessage());
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("404Error");
+        modelAndView.addObject("exception", exception);
         return modelAndView;
     }
 }
--- src/main/java/com/petproject/recipe/service/RecipeServiceImpl.java
+++ src/main/java/com/petproject/recipe/service/RecipeServiceImpl.java
@@ -41,8 +41,7 @@ public class RecipeServiceImpl implements RecipeService{
     public Recipe findById(Long id){
         Optional<Recipe> recipeOptional = recipeRepository.findById(id);
         if(!recipeOptional.isPresent()){
-            //throw new RuntimeException("Recipe Not Found!");
-            throw new NotFoundException("Recipe Not Found!");
+            throw new NotFoundException("Recipe Not Found! For Recipe ID : " + id.toString());
         }
         return recipeOptional.get();
     }
